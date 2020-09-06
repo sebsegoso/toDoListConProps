c845404bed36e0452483250b912259e04428ee47
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2020-09-05 22:40:44 -0400

    Se edita layout y estilos. Se agregan iconos a los botones

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <input v-model="tarea" type="text" autofocus @keyup.13="add" />
-    <button @click="add">Agregar tarea</button>
-    <Tarea
-      v-for="(tarea, i) in tareas "
-      :tarea="tarea"
-      :indice="i"
-      :key="i"
-      @eliminar="eliminarTarea"
-    ></Tarea>
+  <div class="wrapper">
+    <h1>To do list v2</h1>
+    <div class="inputs">
+      <input v-model="tarea" type="text" autofocus @keyup.13="add" />
+      <button @click="add" class="agregar">
+        <i class="far fa-plus-square"></i> Agregar tarea
+      </button>
+    </div>
+    <p>{{mensajeVacío}}</p>
+    <hr>
+    <ul>
+      <Tarea
+        v-for="(tarea, i) in tareas "
+        :tarea="tarea"
+        :indice="i"
+        :key="i"
+        @eliminar="eliminarTarea"
+      ></Tarea>
+    </ul>
   </div>
 </template>
 
@@ -21,6 +30,7 @@ export default {
     return {
       tarea: "",
       tareas: [],
+      mensajeVacío: "",
     };
   },
   components: {
@@ -28,16 +38,56 @@ export default {
   },
   methods: {
     add() {
-      this.tareas.push(this.tarea);
-      this.tarea = "";
+      if (this.tarea.trim() == "") {
+        this.mensajeVacío = "Debes escribir una tarea para poder ingresarla";
+      } else {
+        this.tareas.push(this.tarea.trim());
+        this.tarea = "";
+        this.mensajeVacío = "";
+      }
     },
     eliminarTarea(indice) {
-      this.tareas.splice(indice, 1);
+      let confirmación = confirm("¿Desea eliminar esta tarea?");
+      confirmación ? this.tareas.splice(indice, 1) : false;
     },
   },
 };
 </script>
 
 <style lang="scss">
-
+body {
+  min-height: 100vh;
+  background: linear-gradient(#eee , #ccc);
+  font-family: Helvetica, sans-serif;
+  .wrapper {
+    margin: 0 auto;
+    width: 60%;
+  }
+}
+h1 {
+  font-size: 24px;
+  text-align: center;
+}
+p{
+  margin: 3px;
+}
+.inputs {
+  display: flex;
+  justify-content: center;
+  .agregar {
+    cursor: pointer;
+    color: #fff;
+    border: 0;
+    background-color: #42cb6f;
+    padding: 3px;
+    transition: all 0.3s;
+    border-radius: 0 5px 5px 0;
+    &:hover {
+      background-color: #99d468;
+    }
+    &:active {
+      background-color: #47cec0;
+    }
+  }
+}
 </style>
--- src/components/Tarea.vue
+++ src/components/Tarea.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    {{tarea}}
-    <button @click="vaciar">Eliminar tarea</button>
+    <li>
+        <button @click="vaciar" class="eliminar">
+          <i class="far fa-minus-square"></i>
+        </button>
+        {{tarea}}
+    </li>
   </div>
 </template>
 
@@ -20,5 +24,27 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.eliminar {
+  cursor: pointer;
+  background-color: #ed5564;
+  color: #fff;
+  border-radius: 5px;
+  border: 0;
+  padding: 3px;
+  transition: all 0.3s;
+  width: 20px;
+  height: 20px;
+  &:hover {
+    background-color: #d34855;
+  }
+  &:active {
+    background-color: #f3f5f5;
+  }
+}
+
+li{
+    padding: 5px 0;
+    list-style-type: none;
+}
 </style>
